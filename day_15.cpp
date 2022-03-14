8c7a1ce02a704435bfd58f179011cbdf11e09796
Gustav Andersson <gustav.k.andersson@gmail.com>
2022-03-14 18:04:47 +0100

    Day 15 pt 2

--- day_15.cpp
+++ day_15.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <algorithm>
-#include <numeric>
 #include <memory>
 #include <queue>
 
@@ -16,6 +15,12 @@ struct Node
     int distance{std::numeric_limits<int>::max()};
     bool visited{false};
     std::vector<Node*> neighbours;
+};
+
+struct Node2
+{
+    Node* node;
+    int distance;
 };
 
 template <size_t size>
@@ -65,53 +70,46 @@ void solve(Array2D<Node, size>& map)
     std::cout << "Shortest total distance: " << map.back().back().distance << std::endl;
 }
 
-struct Node2
-{
-    Node* node;
-    int distance;
-};
-
+// Pretty efficient Dijkstra impl. Solves part 2 in ~40s on a 6 year old intel laptop.
 template <size_t size>
-void solve_opt2(Array2D<Node, size>& map)
+void solve_opt(Array2D<Node, size>& map)
 {
     map[0][0].distance = 0;
 
-    // By keeping distance in the temporary node, memory access is much more cache friendly when sorting
     std::vector<Node2> unvisited;
     unvisited.reserve(map.size() * map.size());
     std::for_each(map.begin(), map.end(), [&] (auto& i) {std::for_each(i.begin(), i.end(), [&] (auto& node) {unvisited.push_back({&node, node.distance});});});
 
-    std::reverse(unvisited.begin(), unvisited.end());
+    auto unvisited_front = unvisited.begin();
 
-    while (!unvisited.empty())
+    while (unvisited_front != unvisited.end())
     {
-        auto node = unvisited.back();
-        unvisited.pop_back();
-
-        if (unvisited.size() % 10000 == 0)
-        {
-            std::cout << unvisited.size() <<std::endl;
-        }
+        // Sorting the entire unvisited set is a waste. We just need the single closest node in every iteration.
+        auto nearest = std::min_element(unvisited_front, unvisited.end(), [](const auto& a, const auto& b) {return a.distance < b.distance;});
+        Node2& node = *nearest;
+        node.node->visited = true;
 
         for (auto n : node.node->neighbours)
         {
-            if (!n->visited)
+            if (!n->visited && node.node->distance + n->cost < n->distance)
             {
-                if (node.node->distance + n->cost < n->distance)
-                {
-                    n->distance = node.node->distance + n->cost;
-                    auto base_node = std::find_if(unvisited.begin(), unvisited.end(), [&](Node2& i) {return i.node == n;});
-                    if (base_node != unvisited.end())
-                        base_node->distance = n->distance;
-                }
+                n->distance = node.node->distance + n->cost;
+                auto base_node = std::find_if(unvisited_front, unvisited.end(), [&](Node2& i) {return i.node == n;});
+                if (base_node != unvisited.end())
+                    base_node->distance = n->distance;
             }
         }
-        node.node->visited = true;
-        std::sort(unvisited.begin(), unvisited.end(), [] (const auto& a, const auto& b) {return (a.distance > b.distance);});
+
+        // Don't remove visited nodes, just move them to the front and work on the rest.
+        if (nearest != unvisited_front)
+        {
+            std::iter_swap(nearest, unvisited_front);
+        }
+        unvisited_front++;
+
     }
     std::cout << "Shortest total distance: " << map.back().back().distance << std::endl;
 }
-
 
 int main()
 {
@@ -122,7 +120,7 @@ int main()
     std::cout << "Part 2: " << std::endl;
     auto map_2 = std::make_unique<Array2D<Node, 500>>();
     parse_map(*map_2, DATA_15_12);
-    solve_opt2(*map_2);
+    solve_opt(*map_2);
 
     return 0;
 }
